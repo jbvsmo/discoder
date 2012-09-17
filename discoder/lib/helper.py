3dd13e2921070d4b39312455765baaf6a7b856f5
João Bernardo Oliveira <jbvsmo@gmail.com>
2012-09-17 13:42:25 -0300

    Newer ffmpeg versions use the hh:mm:ss:fff format

--- discoder/lib/helper.py
+++ discoder/lib/helper.py
@@ -1,14 +1,14 @@
 __author__ = 'jb'
 
-def seconds_to_time(num, ff=0):
-    """ Convert a number of seconds to time in the "hh:mm:ss:ff" format
-        where "ff" is the frame value and is zero if not given.
+def seconds_to_time(num, fff=0):
+    """ Convert a number of seconds to time in the "hh:mm:ss.fff" format
+        where "fff" is the frame value and is zero if not given.
         :param num: Number of seconds.
         :type num: int
-        :param ff: Number of frames.
-        :type ff: int
+        :param fff: Number of frames.
+        :type fff: int
         :return str
     """
     mm, ss = divmod(num, 60)
     hh, mm = divmod(mm, 60)
-    return '{0:02}:{1:02}:{2:02}:{3:02}'.format(hh, mm, ss, ff)
+    return '{0:02}:{1:02}:{2:02}.{3:03}'.format(hh, mm, ss, fff)
