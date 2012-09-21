007e9f7d5f400208cc8f4fb564801287295e9f76
Bernardo Vianna <jbvsmo@gmail.com>
2012-09-21 00:05:24 -0300

    changed `Transcoder` class to work with a single file instead of many derivated files. `split` now also do transcoding to solve frame problem.

--- discoder/lib/transcoder.py
+++ discoder/lib/transcoder.py
@@ -1,14 +1,13 @@
-import json
+from __future__ import division
 import os
 import subprocess
 import math
-from discoder.lib import command
-from discoder.lib import parse
+from discoder.lib import command, parse
 
 __author__ = 'jb'
 __metaclass__ = type
 
-
+DEBUG = True
 
 def run(cmd, stdout=None, stderr=None, pipe=True):
     """ Run a command in a list of arguments with Popen.
@@ -25,16 +24,17 @@ def run(cmd, stdout=None, stderr=None, pipe=True):
             stdout = subprocess.PIPE
         if stderr is None:
             stderr = subprocess.PIPE
+    if DEBUG:
+        print(cmd)
     return subprocess.Popen(cmd, stdout=stdout, stderr=stderr).communicate()
-
 
 class Transcoder:
     _probe = None
 
     def __init__(self, filename, flavors=(), outdir=None):
         self.filename = filename
-        self.parts = []
         self.flavors = flavors
+        self.outdir = outdir
         if outdir:
             basename = os.path.basename(filename)
             name, ext = os.path.splitext(basename)
@@ -42,63 +42,70 @@ class Transcoder:
         else:
             self.out = None
 
-        self.files = {
-            'orig': self.conv_original(convert=False),
-            'video': None,
-            'audio': None,
-            'parts': dict.fromkeys(flavors),
-            'final': None,
-        }
-        self.orig_ok = False
+    def __repr__(self):
+        return "<File {0!r}>".format(self.filename.replace('\\', '/'))
 
-    def probe(self, **kw):
-        if self._probe is None:
-            name = self.files['orig'] or self.filename
-            sout, _ = run(command.probe(name, **kw))
-            if kw.get('json'):
+    def as_t(self, filename):
+        """ Return a filename as a Transcoder object with the same
+            flavors and output directory.
+
+            It'll use the type of current `self` object to allow subclassing
+        """
+        return type(self)(filename, self.flavors, self.outdir)
+
+    def probe(self, json=False, **kw):
+        """ Get the `ffprobe` information in a dict-like object
+
+        :param json: Use the `json` print format. Disabled by default
+        :param kw: Other kw arguments to `command.probe` function
+        :return: Parsed output in a dict
+        """
+        if self._probe is None or DEBUG:
+            sout, _ = run(command.probe(self.filename, json=json, **kw))
+            if json:
                 self._probe = json.loads(sout)
             else:
                 self._probe = parse.probe(sout)
         return self._probe
 
-
     def conv_original(self, convert=True):
         output = None if not self.out else self.out.format('{0}', '{1}')
-        cmd = command.conv_original(self.filename, 'libx264', 'aac')
+        cmd = command.convert(self.filename, {}, output=output)
         if convert:
             run(cmd)
-            self.orig_ok = True
-        self.files['orig'] = cmd[-1]
-        return self.files['orig']
+        return self.as_t(cmd[-1])
 
     def separate(self):
         output = None if not self.out else self.out.format('{0}', '.mp4')
-        cmds = command.separate(self.files['orig'], output)
+        cmds = command.separate(self.filename, output)
         for c in cmds:
             run(c)
 
-    def split(self, max_num, min_time):
+    def split(self, max_num, min_time, frames=False, time_to_frames=False):
         output = None if not self.out else self.out.format('{0}', '{1}')
 
-        duration = self.probe().stream[0].duration
-        duration = int(math.ceil(float(duration)))
-        chunks = command.calculate_chunks(duration, max_num, min_time)
+        vprobe = self.probe().streams[0]
 
-        cmds = command.split(self.files['orig'], chunks, output)
+        if frames:
+            fps = vprobe.r_frame_rate
+            if '/' in fps:
+                a, b = fps.split('/')
+                fps = int(a) / int(b)
+            else:
+                fps = float(fps)
+
+            duration = int(vprobe.nb_frames)
+        else:
+            fps = None
+            duration = int(math.ceil(float(vprobe.duration)))
+
+        base_cmds = command.split(duration, max_num, min_time, fps, time_to_frames)
+
+        cmds = []
+        for part, base in enumerate(base_cmds):
+            cmds.append(command.convert(self.filename, self.flavors, base, part=part))
+
         for c in cmds:
             run(c)
 
-        self.parts = [c[-1] for c in cmds]
-        return self.parts
-
-
-    def join(self):
-        if not self.parts:
-            raise Exception('Nothing to join.')
-        ext = 'm2ts'
-        cmds = command.transform_mpg(self.parts, ext)
-        fnames = [c[-1] for c in cmds]
-        for c in cmds:
-            run(c)
-
-        return [c[-1] for c in cmds]
+        return [self.as_t(c[-1]) for c in cmds]
